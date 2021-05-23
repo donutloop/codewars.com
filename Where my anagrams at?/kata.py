2321dfbba3e3765614b79d2699cc23054e73ed0a
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2021-05-23 16:07:48 +0200

    Where my anagrams at?

---
+++ Where my anagrams at?/kata.py
@@ -0,0 +1,11 @@
+def anagrams(word, words):
+    sorted_word_a = sorted(word)
+    sorted_word_a = "".join(sorted_word_a)
+    match = []
+    for i in range(len(words)):
+        sorted_word_b = sorted(words[i])
+        sorted_word_b = "".join(sorted_word_b)
+        if sorted_word_b == sorted_word_a:
+            match.append(words[i])
+
+    return match       
